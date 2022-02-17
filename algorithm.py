6dcf7b86e7842378a820c66f45e4977b5a80964b
Aryan Chawla <aryanchawlad@gmail.com>
2022-02-17 11:30:29 -0500

    Can solve any square matrix

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,7 @@
 """
    Notes
       Assuming Square Matrix
-      LAST ROW METHOD HALF DONE!
+      Can solve any square matrix
 
 
 """
@@ -11,10 +11,11 @@ import random
 
 # Solved board
 solved = [
-   [1,2,3,4],
-   [5,6,7,8],
-   [9,10,11,12],
-   [13,14,15,16]
+   [1,2,3,4,5],
+   [6,7,8,9,10],
+   [11,12,13,14,15],
+   [16,17,18,19,20],
+   [21,22,23,24,25]
 ]
 
 # Slicing array
