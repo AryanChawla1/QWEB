8781d6866e37ab660086d7dbf693e96df67e13c5
Aryan Chawla <aryanchawlad@gmail.com>
2022-02-17 00:40:23 -0500

    50% done Last row for 4 by 4 + dimension

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,9 @@
 """
    Notes
       Assuming Square Matrix
-      3 by 3 Matrix Only
-      Should do everything but last row
-      for other square matrices
-      (UNTESTED)
+      LAST ROW METHOD HALF DONE!
+
+
 """
 
 import random
@@ -12,9 +11,10 @@ import random
 
 # Solved board
 solved = [
-   [1,2,3],
-   [4,5,6],
-   [7,8,9]
+   [1,2,3,4],
+   [5,6,7,8],
+   [9,10,11,12],
+   [13,14,15,16]
 ]
 
 # Slicing array
@@ -90,6 +90,8 @@ def find(desired_number):
 # cor[0] is row, cor[1] is column
 def fix(number, curr_co, des_co):
 
+   print("Solving", number)
+
    if curr_co == des_co:
       return
    
@@ -106,7 +108,8 @@ def fix(number, curr_co, des_co):
    if (curr_co[1] == des_co[1]):
       move_row(curr_co[0], False)
    
-   move_column(des_co[1], True)
+   for i in range(abs(des_co[0] - curr_co[0])):
+      move_column(des_co[1], True)
    # Can be optimized
    while (find(number)[1] != des_co[1]):
       move_row(find(number)[0], True)
@@ -123,9 +126,45 @@ def solve_board():
       des_coor = ((i-1)//len(board),(i-1)%len(board))
       fix(i, coor, des_coor)
    
+   print("Time for Last row")
    # For last row
-   while board != solved:
-      move_row(len(board) - 1, True)
+   solve_last_row(board_length + 1)
+
+
+def solve_last_row(number):
+
+   if len(board) == 3:
+      while board != solved:
+         move_row(len(board) - 1, True)
+      return
+   
+   # Get first in position
+   while find(number)[1] != 0:
+      move_row(len(board) - 1, False)
+   
+   # Now fix others if needed
+   for i in range(number + 1, number + len(board)):
+      print("Solving", i)
+      if (find(i)[1] != ((i-1)%len(board))) or (find(i)[0] != (len(board) - 1)):
+         # Perform algorithm to solve
+
+         # Check if number is in wrong row
+         if find(i)[0] != (len(board) - 1):
+            for j in range(len(board) - (i%len(board))):
+               move_row(len(board) - 1, True)
+            while find(i)[0] != (len(board) - 1):
+               move_column(len(board) - 1, True)
+         elif find(i)[1] != ((i-1)%(len(board))):
+            while find(i)[1] != (len(board) - 1):
+               move_row(len(board) -1, True)
+            move_column(len(board) - 1, False)
+            for j in range(len(board) - (i%len(board))):
+               move_row(len(board) -1, True)
+            move_column(len(board) - 1, True)
+            
+         while find(number)[1] != 0:
+            move_row(len(board) - 1, False)
+
 
 scramble_board()
 solve_board()
