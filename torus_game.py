3189615da0245d2b4b482da271ce9bb16a5fc6c1
= <60534068+Ronkiji@users.noreply.github.com>
2022-02-22 16:14:16 -0500

    python 3 changes

--- torus_game.py
+++ torus_game.py
@@ -5,14 +5,11 @@ shape = random.randint(0, 3)
 
 board = []
 
-if shape == 0:
-    square(random.randint(3, 5), board)
-elif shape == 1:
-    rectangle(random.randint(3, 5), random.randint(3, 5), board)
-elif shape == 2:
-    triangle(random.randint(3, 4), board)
-elif shape == 3: 
-    diamond(random.randrange(5, 8, 2), board)
+match shape:
+    case 0: square(random.randint(3, 5), board)
+    case 1: rectangle(random.randint(3, 5), random.randint(3, 5), board)
+    case 2: triangle(random.randint(3, 4), board)
+    case 3: diamond(random.randrange(5, 8, 2), board)
 
 solved = [row[:] for row in board]
 shuffle(board)
@@ -21,16 +18,12 @@ print_board(board)
 
 while board != solved:
     command = input("Command: ")
-    if command[1] == "u":
-        move_column(int(command[0])-1, False, board)
-    elif command[1] == "d":
-        move_column(int(command[0])-1, True, board)
-    elif command[1] == "l":
-        move_row(int(command[0])-1, False, board)
-    elif command[1] == "r":
-        move_row(int(command[0])-1, True, board)
-    elif command == "end":
-        exit()
+    match command[1]:
+        case "u": move_column(int(command[0])-1, False, board)
+        case "d": move_column(int(command[0])-1, True, board)
+        case "l": move_row(int(command[0])-1, False, board)
+        case "r": move_row(int(command[0])-1, True, board)
+    if command == "end": exit()
 
     print()
     print_board(board)
