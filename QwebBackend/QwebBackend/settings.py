81f33b2aa20a4da1746e77ff4a8cb1192a5c6cf2
Aryan Chawla <aryanchawlad@gmail.com>
2022-02-23 23:49:33 -0500

    Fixing errors.

--- QwebBackend/QwebBackend/settings.py
+++ QwebBackend/QwebBackend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from secret_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = 'django-insecure-c%@=)ph9rwtfko_y#q5)l@4f!j^296ojgxna&erst&1mfajk@('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
