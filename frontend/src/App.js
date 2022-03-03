017a6eda9d7ffa7cf3f981ff6351623219768ba3
Aryan Chawla <aryanchawlad@gmail.com>
2022-03-03 09:53:51 -0500

    Everything on there, looks bad though

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="header_menu">
-        <button className="header">Create Account</button>
-        <button className="header">Sign In</button>
-      </div>
-      <div id="splash">
-        <h1 className="title">Torus</h1>
-        <h1 className="title">Puzzle</h1>
-        <div className="menu_options">
-          <button id="classic" className="menu">Classic</button>
-          <button id="daily" className="menu">Daily</button>
+      <div className="grid-container">
+        <div className="header">
+          <button id="create-account" className="header-option">Create Account</button>
+          <button id="sign-up" className="header-option">Sign Up</button>
+        </div>
+        <div className="menu">
+          <h1 id="torus" className="title">Torus</h1>
+          <h1 id="puzzle" className="title">Puzzle</h1>
+          <button id="classic" className="menu-option">Classic</button>
+          <button id="daily" className="menu-option">Daily</button>
+        </div>
+        <div className="board">
+          <p className="tiles">1</p>
+          <p className="tiles">2</p>
+          <p className="tiles">3</p>
+          <p className="tiles">4</p>
+          <p className="tiles">5</p>
+          <p className="tiles">6</p>
+          <p className="tiles">7</p>
+          <p className="tiles">8</p>
+          <p className="tiles">9</p>
         </div>
       </div>
     </div>
