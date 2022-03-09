81b86c1146ddb7ed8131eb47bce153e8982b1a86
Dylan Smith <dylansmith9001@gmail.com>
2022-03-09 12:29:41 -0500

    Updates

---
+++ precache-manifest.bc3afb486e6257b93e633226c8d6ed3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6640698bd111495c6cf9",
+    "url": "/toruspuzzle/static/css/main.6b8d3a56.chunk.css"
+  },
+  {
+    "revision": "6640698bd111495c6cf9",
+    "url": "/toruspuzzle/static/js/main.6640698b.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/toruspuzzle/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "65e291d53e0425321dfa",
+    "url": "/toruspuzzle/static/js/2.65e291d5.chunk.js"
+  },
+  {
+    "revision": "be41b768c0509368f31f",
+    "url": "/toruspuzzle/static/js/runtime~main.be41b768.js"
+  },
+  {
+    "revision": "9af96a9f0e41baf5fe075a2a7dd268d3",
+    "url": "/toruspuzzle/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/toruspuzzle/precache-manifest.bc3afb486e6257b93e633226c8d6ed3e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/toruspuzzle/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
