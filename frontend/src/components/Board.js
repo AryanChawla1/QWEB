57fa1fd0f39742fecc2547fd7195bbebd29c9031
Dylan Smith <dylansmith9001@gmail.com>
2022-03-11 14:54:24 -0500

    Fixed completion alert

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Tiles from './Tiles'
 
-const Board = ({width, tiles, boardWidth, moveRow, moveCol, completedBoard, setCompletedBoard, setTiles, nonFunctional}) => {
+const Board = ({width, tiles, boardWidth, completedBoard, setCompletedBoard, setTiles, nonFunctional}) => {
   return (   
     <div style={{width: boardWidth, height: boardWidth}}>
         <Tiles width={width} tiles={tiles} boardWidth={boardWidth} setTiles={setTiles}
-        nonFunctional={nonFunctional}/>
+        completedBoard={completedBoard} setCompletedBoard={setCompletedBoard} nonFunctional={nonFunctional}/>
     </div>
   )
 }
