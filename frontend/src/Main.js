1f9d212ac5088b383275dfe0b34199771917df14
Aryan Chawla <aryanchawlad@gmail.com>
2022-03-10 23:33:14 -0500

    Removed unused imports

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import App from './pages/App';
 import Splash from './pages/Splash';
