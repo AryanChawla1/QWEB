19d5ce0dae23e4dc43b0b905c9d9c2ed148355d9
Dylan Smith <dylansmith9001@gmail.com>
2022-03-11 11:56:52 -0500

    functional routing buttons

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import App from './pages/App';
 import Splash from './pages/Splash';
+import Daily from './pages/Daily';
 
 const Main = () => {
    return (
          <Routes>
-            <Route exact path='/' element={<App />} />
-            <Route path = '/splash' element={<Splash />} />
+            <Route exact path='/' element={<Splash />} />
+            <Route path = '/classic' element={<App />} />
+            <Route path = '/daily' element={<Daily />} />
          </Routes>
    );
 }
--- frontend/src/pages/Splash.js
+++ frontend/src/pages/Splash.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Splash.css';
 import Board from '../components/Board';
 import helpers from '../helpers';
+import { Link } from 'react-router-dom';
 
 const Splash = () => {
    const boardWidth = 500
@@ -15,8 +16,12 @@ const Splash = () => {
             <button className="accountStuff">Sign Up</button>
          </div>
          <div className="titleText">TORUS PUZZLE</div>
-         <button id="Splash-dailyBtn" className="button1" >Daily</button>
-         <button id="Splash-classicBtn" className="button1">Classic</button>
+         <Link to="/daily">
+            <button id="Splash-dailyBtn" className="button1">Daily</button>
+         </Link>
+         <Link to="/classic">
+            <button id="Splash-classicBtn" className="button1">Classic</button>
+         </Link>
          <div id="Splash-board" className='board'>
             <Board width={width} tiles={tiles} boardWidth={boardWidth} nonFunctional={true}/>
          </div>
