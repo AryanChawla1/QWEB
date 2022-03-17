680fd0e83acdf9ad6419b137b1e52c54e7d9edb2
Aryan Chawla <aryanchawlad@gmail.com>
2022-03-17 08:58:24 -0400

    Added some css to create-account

--- frontend/src/pages/CreateAccount.js
+++ frontend/src/pages/CreateAccount.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import '../styles/CreateAccount.css'
+import { Link } from 'react-router-dom'
 
 const CreateAccount = () => {
    return (
-      <div>
+      <div className="account">
+         <Link to='/'>
+            <button id="back-button">Back</button>
+         </Link>
+         <button id="sign-in">Sign In</button>
          <form>
+            <h1>Create Account</h1>
             <label for="name">Name</label><br/>
-            <input type="text" id="name" name="name" /><br/>
+            <input type="text" id="name" name="name" required/><br/>
+            <label for="email">Email</label><br/>
+            <input type="email" id="email" name="email" required/><br/>
             <label for="password">Password</label><br/>
-            <input type="passowrd" id="password" name="password" /><br/>
-            <input type="submit" value="Submit" /> 
+            <input type="password" id="password" name="password" required/><br/>
+            <input type="submit" value="Create Account" /> 
          </form>
       </div>
    )
