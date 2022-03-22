aa02a0b4b065ecf49320bfe27a712180ad5ae931
Aryan Chawla <aryanchawlad@gmail.com>
2022-03-22 19:43:58 -0400

    Fixing errors

--- Qbackend/users/serializers.py
+++
@@ -1,7 +0,0 @@
-# users/serializers.py
-from rest_framework.serializers import ModelSerializer
-from .models import CustomUser
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'last_login', 'date_joined', 'is_staff', "streak")
