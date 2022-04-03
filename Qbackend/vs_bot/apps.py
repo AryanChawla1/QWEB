99e833603332e1bc1f7b53a54633dc5acaa86cd8
Aryan Chawla <aryanchawlad@gmail.com>
2022-04-03 16:12:14 -0400

    Some files not added...

---
+++ Qbackend/vs_bot/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VsBotConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vs_bot'
---
+++ Qbackend/vs_bot/views.py
@@ -0,0 +1,17 @@
+from django.http import JsonResponse
+from vs_bot.algorithm import Solve
+from django.http.response import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+# Create your views here.
+
+@api_view(['GET'])
+def solve(request):
+   try:
+      data = request.data["board"]
+      attempt_solve = Solve(data)
+      commands = attempt_solve.solve_board()
+      return JsonResponse({'commands': commands}, status=status.HTTP_200_OK)
+   except:
+      return JsonResponse({'error': 'error'}, status=status.HTTP_400_BAD_REQUEST, safe=False)
